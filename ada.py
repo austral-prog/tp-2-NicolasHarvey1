2ef1ccfa0ded5019886748463ed879e0f36f3f0b
NicolasHarvey1 <162722159+NicolasHarvey1@users.noreply.github.com>
2024-03-23 15:38:43 -0300

    Update ada.py

--- ada.py
+++ ada.py
@@ -2,10 +2,7 @@ def ada():
     first_name = "AdA"
     last_name = "LoVeLAce"
 
-    print(f"{first_name} {last_name}".lower())
-    
-    print(f"{first_name} {last_name}".title())
-    
-    print(f"{first_name} {last_name}".upper())
-    
+    print(f"{first_name} {last_name}".lower())    
+    print(f"{first_name} {last_name}".title())    
+    print(f"{first_name} {last_name}".upper())    
     print(f"\t{first_name} {last_name}".lower())
